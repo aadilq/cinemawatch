614e73f7956069f37df2a02dc4f23eb553b0b274
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-20 14:00:37 -0400

    Set up GET request for movie details

---
+++ cinemawatch/src/app/pages/movie/[id].js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from "react";
+import { api, IMAGE_BASE_URL } from "@/app/api/api";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "@/app/components/Navbar";
+
+const MovieDetailPage = () =>{
+    const router = useRouter();
+    const { movie_id } = router.query;
+    const [movie, setMovie] = useState(null);
+    const [cast, setCast] = useState([]);
+
+    useEffect(()=> {
+        if(!id) return;
+
+        //fetching the movie details
+        api.get(`/movie/${movie_id}`)
+            .then(response => setMovie(response.data))
+            .catch(error => console.error('Error fetching movie details', error));
+
+    })
+
+}
