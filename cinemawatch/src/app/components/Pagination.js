db93c1bfbb33e61a28bc4f45c5bd60626ad2ee32
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-25 14:50:56 -0400

    Define react component for pagination

---
+++ cinemawatch/src/app/components/Pagination.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Pagination = ({currentPage, totalPages, onPageChange}) => {
+    const maxButtons = 5;
+
+    const startPage = Math.max(1, currentPage - Math.floor(maxButtons / 2));
+
+}
