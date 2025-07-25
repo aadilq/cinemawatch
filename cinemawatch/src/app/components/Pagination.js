ae06844e19498c489dd18e6c01a5ad09f6ceb45f
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-25 14:53:24 -0400

    Calculate the starting page number

--- cinemawatch/src/app/components/Pagination.js
+++ cinemawatch/src/app/components/Pagination.js
@@ -4,5 +4,6 @@ const Pagination = ({currentPage, totalPages, onPageChange}) => {
     const maxButtons = 5;
 
     const startPage = Math.max(1, currentPage - Math.floor(maxButtons / 2));
+    
 
 }
