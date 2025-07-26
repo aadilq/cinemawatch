5331bf3a6b01dd0007208db73a16f1be51422c4f
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-25 20:20:44 -0400

    Create pagination for specific page change

--- cinemawatch/src/app/components/Pagination.js
+++ cinemawatch/src/app/components/Pagination.js
@@ -23,6 +23,18 @@ const Pagination = ({currentPage, totalPages, onPageChange}) => {
                         &laquo;
                     </button>
                 </li>
+                {pages.length > 0 && pages.map(page => (
+                    <li
+                    key={page}
+                    className={`page-item ${currentPage == page ? 'active': ''}`}>
+                        <button
+                        className="page-link"
+                        onClick={() => onPageChange(page)}
+                        >
+                            {page}
+                        </button>
+                    </li>
+                ))}
             </ul>
         </nav>
     )
