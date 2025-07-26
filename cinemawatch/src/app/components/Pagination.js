29033595bef0ed2580766ec02ff28c3fc7bafa72
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-25 21:29:32 -0400

    Create pagination for next page change

--- cinemawatch/src/app/components/Pagination.js
+++ cinemawatch/src/app/components/Pagination.js
@@ -35,7 +35,17 @@ const Pagination = ({currentPage, totalPages, onPageChange}) => {
                         </button>
                     </li>
                 ))}
+                 <li className={`page-item ${currentPage == 1 ? 'disabled' : ''}`}>
+                 <button
+                    className="page-link"
+                    onClick={() => onPageChange(currentPage + 1)}
+                    disabled={currentPage === totalPages}>
+                        &raquo;
+                        </button>
+                 </li>
             </ul>
         </nav>
     )
-}
+};
+
+export default Pagination;
