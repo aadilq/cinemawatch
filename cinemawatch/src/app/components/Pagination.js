4e1d1dc9fcf14eacc2d0392c07a9bd56b766245d
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-25 20:06:37 -0400

    Create pagination for previous page

--- cinemawatch/src/app/components/Pagination.js
+++ cinemawatch/src/app/components/Pagination.js
@@ -4,6 +4,26 @@ const Pagination = ({currentPage, totalPages, onPageChange}) => {
     const maxButtons = 5;
 
     const startPage = Math.max(1, currentPage - Math.floor(maxButtons / 2));
-    
 
+    const endPage = Math.min(totalPages, startPage + maxButtons - 1);
+
+    const adjustedStartPage = Math.max(1, endPage - maxButtons + 1);
+
+    const pages = Array.from({ length: endPage - adjustedStartPage + 1}, 
+        (_, i) => adjustedStartPage + 1)
+
+    return(
+        <nav aria-label="Page navigation">
+            <ul className="pagination justify-content-center">
+                <li className={`page-item ${currentPage == 1 ? 'disabled' : ''}`}>
+                    <button
+                    className="page-link"
+                    onClick={() => onPageChange(currentPage - 1)}
+                    disabled={currentPage === 1}>
+                        &laquo;
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    )
 }
