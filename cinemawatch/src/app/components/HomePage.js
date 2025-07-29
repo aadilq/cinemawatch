944bd8c152de4157b2b4c56614feed3a64e808c7
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-07-29 12:50:31 -0400

    Configure async function to fetch movies

---
+++ cinemawatch/src/app/components/HomePage.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+import { api } from "../api/api";
+import MovieCard from "./MovieCard";
+import Pagination from "./Pagination";
+import Navbar from "./Navbar";
+
+const HomePage = () =>{
+    const [movies, setMovies] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [query, setQuery] = useState('');
+    const [searching, setSearching] = useState(false);
+
+    useEffect(() =>{
+        const fetchMovie = async () => {
+            try {
+                const endpoint = searching ? '/search/movie' : '/movie/popular'
+                const response = api.get(endpoint, {
+                    params: {
+                        page: currentPage, 
+                        query: searching ? query : undefined
+                    }
+                });
+                setMovies((await response).data.result);
+                setTotalPages(((await response).data.total_Pages))
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    })
+}
